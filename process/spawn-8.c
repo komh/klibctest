c8b40d3b76fb8f6da73b6a8fbaea01b3fe5d0f7d
KO Myung-Hun <komh78@gmail.com>
2025-12-18 13:34:24 +0900

    process: add a test for spawn() to return the correct exit code
    spawn() returns 0 not 3 as the exit code when a child abort()s.
    
        new file:   process/spawn-8.c

---
+++ process/spawn-8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
+
+#define ABORT_RC 3
+
+int child( int argc, char *argv[])
+{
+    abort();
+
+    /* NOT REACH HERE */
+
+    exit( ABORT_RC );
+
+    return 0;
+}
+
+int main( int argc, char *argv[])
+{
+    int rc;
+    int failed;
+
+    if( argc > 1 )
+        return child( argc, argv );
+
+    printf("Testing to return the correct exit code when a child abort()s...\n");
+    rc = spawnl( P_WAIT, argv[ 0 ], argv[ 0 ], argv[ 0 ], NULL );
+
+    failed = rc != ABORT_RC;
+    printf("%s: rc = %d(%d)\n", failed ? "FAILED" : "PASSED", rc, ABORT_RC );
+
+    return failed;
+}
